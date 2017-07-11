97cd22ba081e94b10cfcfcb8b3c75cbb7757294c
t-kusakabe <ttmac0704@gmail.com>
2017-07-11 18:28:43 +0900

    fix assosiation

--- app/models/gather.rb
+++ app/models/gather.rb
@@ -1,4 +1,4 @@
 class Gather < ApplicationRecord
   has_many :gathers_users_relation, dependent: :destroy
-  has_many :users :through => :gathers_users_relation
+  has_many :users, through: :gathers_users_relation
 end
