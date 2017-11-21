0b12b3217e3df7d6b13a82fa8e1e4865486d4d90
t-kusakabe <ttmac0704@gmail.com>
2017-11-21 15:40:43 +0900

    Add HMR

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
+// import './application.css'
+import injectTapEventPlugin from 'react-tap-event-plugin'
+injectTapEventPlugin()
+
 ReactDOM.render(
-  <div>Hello React!</div>,
+  <div>Hello React!aaaa</div>,
   document.getElementById('app')
 )
