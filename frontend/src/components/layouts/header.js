737d8c3274aa8d14b9688b71481c867683788771
t-kusakabe <ttmac0704@gmail.com>
2017-11-24 19:18:31 +0900

    Add list in header

--- frontend/src/components/layouts/header.js
+++ frontend/src/components/layouts/header.js
@@ -13,6 +13,11 @@ export default class Header extends React.Component {
     return(
       <div className='header'>
         <h1>OIC::Gather</h1>
+        <ul>
+          <li>Gather投稿</li>
+          <li>通知</li>
+          <li>アイコン</li>
+        </ul>
       </div>
     )
   }
