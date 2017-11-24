7831cba0474e2bc7934dd5597fcd51ad54ad0ccf
t-kusakabe <ttmac0704@gmail.com>
2017-11-24 14:01:09 +0900

    Create top screen

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,10 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 
 // import './application.css'
+import Top from './components/Top.jsx'
+
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
 ReactDOM.render(
-  <div>Hello React!</div>,
+  <div className='contents'>
+    <div>Hello React!</div>
+    <Top />
+  </div>,
   document.getElementById('app')
 )
