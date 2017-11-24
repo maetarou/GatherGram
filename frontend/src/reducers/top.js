06278ce3bf598c1bda1d7bbb6e3781a5797bbf90
t-kusakabe <ttmac0704@gmail.com>
2017-11-24 16:30:19 +0900

    Create reducer top

---
+++ frontend/src/reducers/top.js
@@ -0,0 +1,11 @@
+const initialState = {
+  top: 'test'
+}
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default: {
+      return state
+    }
+  }
+}
