7f23037d88c82e35a1174de8fa172cfef39f2d48
t-kusakabe <ttmac0704@gmail.com>
2017-11-24 16:30:36 +0900

    Add redux in app.js

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -1,16 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-// import './application.css'
-import Top from './components/Top.jsx'
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+import Top from './containers/top'
 
 import injectTapEventPlugin from 'react-tap-event-plugin'
 injectTapEventPlugin()
 
+const store = configureStore()
+
 ReactDOM.render(
-  <div className='contents'>
-    <div>Hello React!</div>
-    <Top />
-  </div>,
+  <Provider store={store}>
+    <div className='contents'>
+      <h1 className='title'>Hello, React!</h1>
+      <Top />
+    </div>
+  </Provider>,
   document.getElementById('app')
 )
