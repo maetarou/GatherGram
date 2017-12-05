982af7f4a495581b03717294f07e045a5ef917a0
t-kusakabe <ttmac0704@gmail.com>
2017-12-05 15:06:48 +0900

    Delete useless file

--- app/models/gathers_users_relation.rb
+++
@@ -1,4 +0,0 @@
-class GathersUsersRelation < ApplicationRecord
-  belongs_to :user
-  belongs_to :gather
-end
--- app/models/user.rb
+++
@@ -1,7 +0,0 @@
-class User < ApplicationRecord
-  has_many :gathers_users_relation, dependent: :destroy
-  has_many :gathers, through: :gathers_users_relation
-
-  devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
-end
