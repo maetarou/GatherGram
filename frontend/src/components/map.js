281e8c22ae7732874532ba807eac64969ca4863f
t-kusakabe <ttmac0704@gmail.com>
2017-12-12 23:41:01 +0900

    Create map component

--- frontend/src/components/map.js
+++ frontend/src/components/map.js
@@ -1,5 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import GoogleMap from 'google-map-react'
+
+const MarkerComponent = ({text}) => {
+  return(
+    <div className='p-marker'>
+    {text}
+      <img src='../../images/marker.png'></img>
+    </div>
+  )
+}
+
+
 
 export default class Map extends React.Component {
   constructor(props) {
@@ -12,7 +24,18 @@ export default class Map extends React.Component {
   render() {
     return(
       <div className='p-map'>
-        mapを埋め込む
+        <GoogleMap
+          defaultCenter = {{
+            lat: 34.663776,
+            lng: 135.518523
+          }}
+          defaultZoom = {19}
+        >
+          <MarkerComponent
+            lat={34.663776}
+            lng={135.518523}
+          />
+        </GoogleMap>
       </div>
     )
   }
