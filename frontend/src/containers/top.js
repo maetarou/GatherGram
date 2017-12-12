3ead3a1b4ea3d2ce2fe373c8c21e1403cae21a43
t-kusakabe <ttmac0704@gmail.com>
2017-12-13 01:24:50 +0900

    Change file

--- frontend/src/containers/top.js
+++ frontend/src/containers/top.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-import Top from '../components/top'
+import Top from '../components/tops/top'
 
 function mapStateToProps(state) {
   return state
