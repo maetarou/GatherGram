2fc83a51d94212f44aa363e66ef8d14811511194
t-kusakabe <ttmac0704@gmail.com>
2017-12-15 19:50:43 +0900

    Delete redux

--- frontend/src/app.js
+++ frontend/src/app.js
@@ -2,25 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 
-// Store
-import configureStore from './store/configureStore'
-
 // Container
-import Header from './containers/layouts/header'
-import Sidebar from './containers/layouts/sidebar'
-import Top from './containers/top'
-
-const store = configureStore()
+import Header from './components/layouts/header'
+import Sidebar from './components/layouts/sidebar'
+import Top from './components/top/top'
 
 ReactDOM.render(
-  <Provider store={store}>
-    <div>
-      <Header />
-      <Sidebar />
-      <div className='l-contents'>
-        <Top />
-      </div>
+  <div>
+    <Header />
+    <Sidebar />
+    <div className='l-contents'>
+      <Top />
     </div>
-  </Provider>,
+  </div>,
   document.getElementById('app')
 )
