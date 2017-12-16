ea09ed40d3acb97642f4ad0c548a4a7182ea3e8f
r-miyamae <b5005@oic.jp>
2017-12-17 00:55:43 +0900

    modify migration file

--- db/migrate/20171216151246_add_columns_to_users.rb
+++ db/migrate/20171216151246_add_columns_to_users.rb
@@ -1,7 +1,6 @@
 class AddColumnsToUsers < ActiveRecord::Migration[5.1]
   def change
     add_column :users, :uid, :string
-    add_column :users, :gather, :string
-    add_column :users, :int, :string
+    add_column :users, :gather, :integer
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -26,8 +26,7 @@ ActiveRecord::Schema.define(version: 20171216151246) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "uid"
-    t.string "gather"
-    t.string "int"
+    t.integer "gather"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
