44abf4d22dbffb500721d76f8e09bbe2355e6424
t-kusakabe <ttmac0704@gmail.com>
2018-01-11 22:39:53 +0900

    Create testData in mapping

--- frontend/src/components/tops/map.js
+++ frontend/src/components/tops/map.js
@@ -16,6 +16,7 @@ export default class Map extends React.Component {
     this.state = {
       data: []
     }
+    console.log(testData)
   }
 
   render() {
@@ -31,13 +32,48 @@ export default class Map extends React.Component {
           }}
           defaultZoom = {19}
         >
-          <MarkerComponent
-            lat={34.663776}
-            lng={135.518523}
-            text='大阪情報コンピュータ専門学校'
-          />
+
+          {testData.map((location) => {
+            console.log(location.caption)
+            return  <MarkerComponent
+              lat={location.location.latitude}
+              lng={location.location.longitude}
+              test={location.caption}
+              key={location.caption}
+            />
+          })}
         </GoogleMap>
       </div>
     )
   }
 }
+
+
+
+const testData = 
+  [
+    {
+        "caption": null, 
+        "imageLink": "https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/25013709_1702304773160676_2010666811950891008_n.jpg", 
+        "link": "https://www.instagram.com/p/Bcb57QJnK2c/", 
+        "location": {
+            "id": 360369503, 
+            "latitude": 34.663698386286, 
+            "longitude": 135.51853980151, 
+            "name": "大阪情報コンピュータ専門学校"
+        }, 
+        "username": "gathergram3203"
+    }, 
+    {
+        "caption": "ゲロ可愛い", 
+        "imageLink": "https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/24331821_149468235685752_4414169010879332352_n.jpg", 
+        "link": "https://www.instagram.com/p/Bcbu2WAnix-/", 
+        "location": {
+            "id": 1032309971, 
+            "latitude": 34.6779594, 
+            "longitude": 135.4979939, 
+            "name": "ことりカフェ心斎橋"
+        }, 
+        "username": "gathergram3203"
+    }, 
+  ]
