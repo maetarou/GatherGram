543fd0168eaa5c1d5e1cea3ec4b1ee05f05b6528
t-kusakabe <ttmac0704@gmail.com>
2018-01-12 17:08:25 +0900

    Modify fetch get contents data Delete test data

--- frontend/src/components/tops/map.js
+++ frontend/src/components/tops/map.js
@@ -14,25 +14,23 @@ export default class Map extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: this.getData()
+      data: []
     }
-
-    this.getData = this.getData.bind(this)
+    this.getData()
   }
 
   getData() {
-    request
-      .get('http://localhost:3000/index')
-      .end(function(err, res) {
-      })
-
-    return(<div />)
+    return fetch('http://localhost:3000/index')
+            .then((response) => {
+              response.json().then((res) => {
+                this.setState({ data: res.contents})
+              })
+            })
   }
 
   render() {
     return(
       <div className='p-map'>
-        {console.log(this.state)}
         <GoogleMap
           bootstrapURLKeys={{
             key: 'AIzaSyAEaUM4truzWM4M4Ob8s9_oUdMrhmxnzcI'
@@ -43,8 +41,7 @@ export default class Map extends React.Component {
           }}
           defaultZoom = {19}
         >
-
-          {testData.map((content) => {
+          {this.state.data.map((content) => {
             return(
               <MarkerComponent
                 lat={content.location.latitude}
@@ -59,31 +56,3 @@ export default class Map extends React.Component {
     )
   }
 }
-
-const testData = 
-  [
-    {
-        "caption": null, 
-        "imageLink": "https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/25013709_1702304773160676_2010666811950891008_n.jpg", 
-        "link": "https://www.instagram.com/p/Bcb57QJnK2c/", 
-        "location": {
-            "id": 360369503, 
-            "latitude": 34.663698386286, 
-            "longitude": 135.51853980151, 
-            "name": "大阪情報コンピュータ専門学校"
-        }, 
-        "username": "gathergram3203"
-    }, 
-    {
-        "caption": "ゲロ可愛い", 
-        "imageLink": "https://scontent.cdninstagram.com/t51.2885-15/s320x320/e35/24331821_149468235685752_4414169010879332352_n.jpg", 
-        "link": "https://www.instagram.com/p/Bcbu2WAnix-/", 
-        "location": {
-            "id": 1032309971, 
-            "latitude": 34.6779594, 
-            "longitude": 135.4979939, 
-            "name": "ことりカフェ心斎橋"
-        }, 
-        "username": "gathergram3203"
-    }, 
-  ]
