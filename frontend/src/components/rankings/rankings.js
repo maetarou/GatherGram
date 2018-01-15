7f1c5b5274702e0815fa951a85c8f10dde05cbd1
t-kusakabe <ttmac0704@gmail.com>
2018-01-16 06:54:08 +0900

    Add ranking component

--- frontend/src/components/rankings/rankings.js
+++ frontend/src/components/rankings/rankings.js
@@ -4,13 +4,35 @@ export default class Ranking extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: []
+      users: []
     }
+
+    this.getRankingData()
+  }
+
+  getRankingData() {
+    return fetch('http://localhost:3000/ranking')
+             .then((response) => {
+               response.json().then((res) => {
+                 this.setState({users: res.top_users})
+               })
+             })
   }
 
   render() {
     return(
-      <div>Welcom Ranking Page</div>
+      <div>
+        {console.log(this.state.users)}
+        {this.state.users.map((user) => {
+          return(
+            <ul key={user.id}>
+              <li>{user.name}</li>
+              <li>{user.gather}</li>
+            </ul>
+          )
+          console.log(user.id)
+        })}
+      </div>
     )
   }
 }
