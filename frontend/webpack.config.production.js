cdbadbc7f2ba40728bec48b3db75c109389ff287
t-kusakabe <ttmac0704@gmail.com>
2018-01-29 00:06:51 +0900

    Create webpack

---
+++ frontend/webpack.config.production.js
@@ -0,0 +1,11 @@
+const merge = require('webpack-merge')
+const baseConfig = require('./webpack.config.base.js')
+
+const config = merge(baseConfig, {
+  output: {
+    path: __dirname + '../public/assets',
+    filename: 'bundle.js'
+  }
+})
+
+module.exports = config
