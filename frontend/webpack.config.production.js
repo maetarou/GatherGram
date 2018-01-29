e8a5da2e7af1f410913c4a0d97ebbacf095740ff
t-kusakabe <ttmac0704@gmail.com>
2018-01-29 16:22:57 +0900

    Modify dist path

--- frontend/webpack.config.production.js
+++ frontend/webpack.config.production.js
@@ -3,7 +3,7 @@ const baseConfig = require('./webpack.config.base.js')
 
 const config = merge(baseConfig, {
   output: {
-    path: __dirname + '../public/assets',
+    path: __dirname + './../public',
     filename: 'bundle.js'
   }
 })
