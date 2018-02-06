e87adf138d5496fb34b8e64cf6f297148bc7561f
t-kusakabe <ttmac0704@gmail.com>
2018-02-06 22:24:32 +0900

    Modify component

--- frontend/src/components/tops/restaurant.js
+++ frontend/src/components/tops/restaurant.js
@@ -4,21 +4,22 @@ export default class Restaurant extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      data: []
+      gnavi: props.gnavi
     }
+
+    console.log(this.state.gnavi)
   }
 
   render() {
     return(
       <ul>
-        {console.log(this.props)}
-        <li>{this.props.gnavi.rest.name}</li>
-        <li>{this.props.gnavi.rest.address}</li>
-        <li>{this.props.gnavi.rest.tel}</li>
-        <li>{this.props.gnavi.rest.opentime}</li>
-        <li>{this.props.gnavi.rest.holiday}</li>
-        <li>{this.props.gnavi.rest.url}</li>
-
+        {console.log(this.state)}
+        <li>{!this.props.gnavi.rest.name}</li>
+        <li>{!this.props.gnavi.rest.address}</li>
+        <li>{!this.props.gnavi.rest.tel}</li>
+        <li>{!this.props.gnavi.rest.opentime}</li>
+        <li>{!this.props.gnavi.rest.holiday}</li>
+        <li>{!this.props.gnavi.rest.url}</li>
       </ul>
     )
   }
