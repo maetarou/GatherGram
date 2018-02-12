c5dd37f0332d46477129e780640ed0549247c182
t-kusakabe <ttmac0704@gmail.com>
2018-02-12 19:53:25 +0900

    Modify Header component to function type

--- frontend/src/components/layouts/header.js
+++ frontend/src/components/layouts/header.js
@@ -4,20 +4,18 @@ import { connect } from 'react-redux'
 import Sidebar from './sidebar'
 import Score from './score'
 
-class Header extends React.Component {
-  render() {
-    return(
-      <div className='l-header'>
-        <img src='http://xn--hckp3ac2l.jp/wp-content/uploads/2016/07/sr-honoka-smile-7ga16-mae.jpg' className='l-header__logo' />
-        <Score />
-        <Sidebar className='l-header__sidebar' />
-      </div>
-    )
-  }
+const HeaderComponent = (props) => {
+  return(
+    <div className='l-header'>
+      <img src='http://xn--hckp3ac2l.jp/wp-content/uploads/2016/07/sr-honoka-smile-7ga16-mae.jpg' className='l-header__logo' />
+      <Score />
+      <Sidebar className='l-header__sidebar' />
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
   return {state: state.header}
 }
 
-export default connect(mapStateToProps)(Header)
+export default connect(mapStateToProps)(HeaderComponent)
