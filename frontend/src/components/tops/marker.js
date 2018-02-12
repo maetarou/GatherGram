446e729ffb05b77784da28252df750c9ad1d2e52
t-kusakabe <ttmac0704@gmail.com>
2018-02-12 19:59:50 +0900

    Modify Marker component to function type

--- frontend/src/components/tops/marker.js
+++ frontend/src/components/tops/marker.js
@@ -3,40 +3,40 @@ import { connect } from 'react-redux'
 
 import Content from './content'
 
-class Marker extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      data: [],
-      hideContent: false
-    }
-
-    this.handleClick = this.handleClick.bind(this)
-  }
-
-  handleClick() {
-    this.setState({
-      hideContent: (this.state.hideContent ? false : true)
-    })
-  }
-
-  render() {
-    return(
-      <div className='p-marker' >
-        <img src={this.props.content.imageLink} className='p-marker__img' onClick={this.handleClick} />
-        <img src='./images/Arrow.png' className='p-marker__arrow' />
-        {
-          this.state.hideContent ? 
-          <Content content={this.props.content} handleClick={this.handleClick} /> :
-          null
-        }
-      </div>
-    )
-  }
+const MarkerComponent = (props) => {
+  return(
+    <div className='p-marker' >
+      <img src={this.props.content.imageLink} className='p-marker__img' onClick={this.handleClick} />
+      <img src='./images/Arrow.png' className='p-marker__arrow' />
+      {
+        this.state.hideContent ? 
+        <Content content={this.props.content} handleClick={this.handleClick} /> :
+        null
+      }
+    </div>
+  )
 }
+
+// class Marker extends React.Component {
+//   constructor(props) {
+//     super(props)
+//     this.state = {
+//       data: [],
+//       hideContent: false
+//     }
+// 
+//     this.handleClick = this.handleClick.bind(this)
+//   }
+// 
+//   handleClick() {
+//     this.setState({
+//       hideContent: (this.state.hideContent ? false : true)
+//     })
+//   }
+// }
 
 const mapStateToProps = (state) => {
   return {state: state.map}
 }
 
-export default connect(mapStateToProps)(Marker)
+export default connect(mapStateToProps)(MarkerComponent)
