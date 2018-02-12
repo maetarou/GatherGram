15877dd463037c49d35327b22418f95f81e69618
t-kusakabe <ttmac0704@gmail.com>
2018-02-12 20:00:05 +0900

    Modify Restrant component to function type

--- frontend/src/components/tops/restaurant.js
+++ frontend/src/components/tops/restaurant.js
@@ -1,24 +1,22 @@
 import React from 'react'
 import { connect } from 'react-redux'
 
-class Restaurant extends React.Component {
-  render() {
-    return(
-      <ul>
-        {console.log(this.state)}
-        <li>{!this.props.gnavi.rest.name}</li>
-        <li>{!this.props.gnavi.rest.address}</li>
-        <li>{!this.props.gnavi.rest.tel}</li>
-        <li>{!this.props.gnavi.rest.opentime}</li>
-        <li>{!this.props.gnavi.rest.holiday}</li>
-        <li>{!this.props.gnavi.rest.url}</li>
-      </ul>
-    )
-  }
+
+const RestaurantComponent = (props) => {
+  return(
+    <ul>
+      <li>{!this.props.gnavi.rest.name}</li>
+      <li>{!this.props.gnavi.rest.address}</li>
+      <li>{!this.props.gnavi.rest.tel}</li>
+      <li>{!this.props.gnavi.rest.opentime}</li>
+      <li>{!this.props.gnavi.rest.holiday}</li>
+      <li>{!this.props.gnavi.rest.url}</li>
+    </ul>
+  )
 }
 
 const mapStateToProps = (state) => {
   return {state: state.score}
 }
 
-export default connect(mapStateToProps)(Restaurant)
+export default connect(mapStateToProps)(RestaurantComponent)
