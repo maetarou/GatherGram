31b5dd41c7d16727c283137a5a51bde0dd932bc5
t-kusakabe <ttmac0704@gmail.com>
2018-02-12 19:54:31 +0900

    Modify Ranking component to function type

--- frontend/src/components/rankings/rankings.js
+++ frontend/src/components/rankings/rankings.js
@@ -3,39 +3,37 @@ import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 import FontAwesome from 'react-fontawesome'
 
-class Ranking extends React.Component {
-  render() {
-    return(
-      <div>
-        {this.state.users.map((user) => {
-          return(
-            <ul key={user.id} className='p-ranking' >
-              <img
-                src='http://suzakijunichi.com/blog/wp-content/uploads/2016/11/1.jpg'
-                className='p-ranking__img'
-              />
-              <li className='p-ranking__gather'>{user.gather}</li>
-              <li>{user.name}</li>
-            </ul>
-          )
-        })}
+const RankingComponent = (props) => {
+  return(
+    <div>
+      {this.state.users.map((user) => {
+        return(
+          <ul key={user.id} className='p-ranking' >
+            <img
+              src='http://suzakijunichi.com/blog/wp-content/uploads/2016/11/1.jpg'
+              className='p-ranking__img'
+            />
+            <li className='p-ranking__gather'>{user.gather}</li>
+            <li>{user.name}</li>
+          </ul>
+        )
+      })}
 
-        <div className='p-ranking__footer'>
-          <div className='p-ranking__footer__rank'>
-            You're rank : 1位
-          </div>
-
-          <Link to='/' className='p-ranking__footer__button'>
-            <FontAwesome name='mail-reply' />
-          </Link>
+      <div className='p-ranking__footer'>
+        <div className='p-ranking__footer__rank'>
+          You're rank : 1位
         </div>
+
+        <Link to='/' className='p-ranking__footer__button'>
+          <FontAwesome name='mail-reply' />
+        </Link>
       </div>
-    )
-  }
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
   return {state: state.ranking}
 }
 
-export default connect(mapStateToProps)(Ranking)
+export default connect(mapStateToProps)(RankingComponent)
