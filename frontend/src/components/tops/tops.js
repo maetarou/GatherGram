79761d15f5a6ed3a73eb587737492ea9e7b540c2
t-kusakabe <ttmac0704@gmail.com>
2018-02-12 20:00:29 +0900

    Modify Top component to function type

--- frontend/src/components/tops/tops.js
+++ frontend/src/components/tops/tops.js
@@ -4,18 +4,16 @@ import { connect } from 'react-redux'
 import Map from './map'
 import User from './user'
 
-class Top extends React.Component {
-  render() {
-    return(
-      <div className='top'>
-        <Map />
-      </div>
-    )
-  }
+const TopComponent = (props) => {
+  return(
+    <div className='top'>
+      <Map />
+    </div>
+  )
 }
 
 const mapStateToProps = (state) => {
   return {state: state.tops}
 }
 
-export default Top
+export default connect(mapStateToProps)(TopComponent)
