95bf7b851a181da4ac1fc6899ec0f7b0c652f21b
t-kusakabe <ttmac0704@gmail.com>
2018-02-15 05:56:08 +0900

    Load files

--- front/src/components/layout/header.js
+++ front/src/components/layout/header.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
+import Score from './score'
+
 const HeaderComponent = (props) => {
   return(
     <div className='header'>
@@ -9,6 +11,7 @@ const HeaderComponent = (props) => {
         src='https://rr.img.naver.jp/mig?src=http%3A%2F%2Fcdn.mkimg.carview.co.jp%2Fminkara%2Fuserstorage%2F000%2F009%2F818%2F181%2F73ea5d0eff.jpg&twidth=1200&theight=1200&qlt=80&res_format=jpg&op=r'
         className='header__img'
       />
+      <Score />
     </div>
   )
 }
--- front/src/reducers/app.js
+++ front/src/reducers/app.js
@@ -1,12 +1,14 @@
 import { combineReducers } from 'redux'
 
 import Header from './layout/header'
+import Score from './layout/score'
 import Map from './top/map'
 import Marker from './top/marker'
 import Content from './top/content'
 
 const main = combineReducers({
   Header,
+  Score,
   Map,
   Marker,
   Content
