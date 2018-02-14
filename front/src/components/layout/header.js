a601c60afdf9a08bf57f99b29b4dcaf2768fa64d
t-kusakabe <ttmac0704@gmail.com>
2018-02-15 07:50:35 +0900

    Modify header component to class

--- front/src/components/layout/header.js
+++ front/src/components/layout/header.js
@@ -4,20 +4,35 @@ import { Link } from 'react-router-dom'
 
 import Score from './score'
 
-const HeaderComponent = (props) => {
-  return(
-    <div className='header'>
-      <img
-        src='https://rr.img.naver.jp/mig?src=http%3A%2F%2Fcdn.mkimg.carview.co.jp%2Fminkara%2Fuserstorage%2F000%2F009%2F818%2F181%2F73ea5d0eff.jpg&twidth=1200&theight=1200&qlt=80&res_format=jpg&op=r'
-        className='header__img'
-      />
-      <Score />
-    </div>
-  )
+class Header extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  handleClick() {
+    this.props.dispatch({
+      type: 'CLICK_SIDEBAR'
+    })
+  }
+
+  render() {
+    return(
+      <div className='header'>
+        <img
+          src='https://rr.img.naver.jp/mig?src=http%3A%2F%2Fcdn.mkimg.carview.co.jp%2Fminkara%2Fuserstorage%2F000%2F009%2F818%2F181%2F73ea5d0eff.jpg&twidth=1200&theight=1200&qlt=80&res_format=jpg&op=r'
+          className='header__img'
+          onClick={this.handleClick}
+        />
+        <Score />
+      </div>
+    )
+  }
 }
 
 const mapStateToProps = (state) => {
   return {state: state.Header}
 }
 
-export default connect(mapStateToProps)(HeaderComponent)
+export default connect(mapStateToProps)(Header)
