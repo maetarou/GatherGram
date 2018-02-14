e14636b771817bf46e668f19833bf9f2f1e9ede0
t-kusakabe <ttmac0704@gmail.com>
2018-02-15 02:32:31 +0900

    Modify map

--- front/src/components/top/map.js
+++ front/src/components/top/map.js
@@ -2,20 +2,70 @@ import React from 'react'
 import { connect } from 'react-redux'
 import GoogleMap from 'google-map-react'
 
-const Map = (props) => {
+import configureStore from '../../store/configureStore'
+import Content from './content'
+
+const store = configureStore()
+
+class Map extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.fetchContents()
+  }
+
+  fetchContents() {
+    fetch('http://localhost:3000/index')
+      .then((response) => {
+        response.json().then((res) => {
+          this.props.dispatch({
+            type: 'GET_CONTENTS',
+            value: res.contents
+          })
+        })
+    })
+  }
+
+  render() {
+    return(
+      <div className='map'>
+        <GoogleMap
+          bootstrapURLKeys={{
+            key: 'AIzaSyAEaUM4truzWM4M4Ob8s9_oUdMrhmxnzcI'
+          }}
+          defaultCenter = {{
+            lat: 34.663776,
+            lng: 135.518523
+          }}
+          defaultZoom = {19}
+        >
+          {console.log(this.props)}
+          {/* コンテンツのマッピング */}
+          {this.props.state.contents.map((content, idx) => {
+            return(
+              <Marker
+                lat={34.663776}
+                lng={135.518523}
+                key={idx}
+                content={content}
+              />
+            )
+          })}
+        </GoogleMap>
+      </div>
+    )
+  }
+}
+
+
+
+const Marker = (props) => {
   return(
-    <div className='map'>
-      <GoogleMap
-        bootstrapURLKeys={{
-          key: 'AIzaSyAEaUM4truzWM4M4Ob8s9_oUdMrhmxnzcI'
-        }}
-        defaultCenter = {{
-          lat: 34.663776,
-          lng: 135.518523
-        }}
-        defaultZoom = {19}
-      >
-      </GoogleMap>
+    <div className='map__marker'>
+      <img
+        src={props.content.media.image_link}
+      />
+      <Content />
     </div>
   )
 }
--- front/src/reducers/top/map.js
+++ front/src/reducers/top/map.js
@@ -1,9 +1,13 @@
 const initialState = {
-  map: ''
+  contents: []
 }
 
 export default (state = initialState, action) => {
   switch(action.type) {
+    case 'GET_CONTENTS':
+      return Object.assign({}, state, {
+        contents: action.value
+      })
     default:
       return state
   }
