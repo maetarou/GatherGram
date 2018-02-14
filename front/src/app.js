bfde2973cb62a09b9f1c85f07b8634c3fd4127ff
t-kusakabe <ttmac0704@gmail.com>
2018-02-15 08:52:03 +0900

    Add commentout

--- front/src/app.js
+++ front/src/app.js
@@ -19,6 +19,14 @@ import Map from './components/top/map'
 
 const store = configureStore()
 
+// const get_gather = () => {
+//   fetch('http://localhost:3000/user/get_gather')
+//     .then((response) => {
+//     })
+// }
+// 
+// get_gather()
+
 ReactDOM.render(
   <Provider store={store}>
     <HashRouter>
