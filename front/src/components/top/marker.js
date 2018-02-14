14f071ee42f88a45b7c038e259957a3c98c1768f
t-kusakabe <ttmac0704@gmail.com>
2018-02-15 04:24:39 +0900

    Create marker

---
+++ front/src/components/top/marker.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import { connect } from 'react-redux'
+
+import Content from './content'
+
+class Marker extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.handleCLick = this.handleCLick.bind(this)
+  }
+
+  handleCLick() {
+    this.props.dispatch({
+      type: 'CLICK_EVENT'
+    })
+  }
+
+  render() {
+    return(
+    <div className='marker' onClick={this.handleCLick}>
+      <img
+        src={this.props.content.media.image_link}
+        className='marker__img'
+      />
+      {
+        this.props.state.hideFlg ?
+        <Content /> :
+        null
+      }
+    </div>
+    )
+  }
+}
+
+const mapStateToProps = (state) => {
+  return {state: state.Marker}
+}
+
+export default connect(mapStateToProps)(Marker)
