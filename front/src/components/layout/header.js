812fe63d99fd5e24003cef83cf179678d38d111e
t-kusakabe <ttmac0704@gmail.com>
2018-02-15 14:28:27 +0900

    Modify user profile image to dynamic

--- front/src/components/layout/header.js
+++ front/src/components/layout/header.js
@@ -4,11 +4,16 @@ import { Link } from 'react-router-dom'
 
 import Score from './score'
 
+const url = document.location.hostname == 'localhost' ?
+            'http://localhost:3000/user' :
+            'https://gathergram.herokuapp.com/user'
+
 class Header extends React.Component {
   constructor(props) {
     super(props)
 
     this.handleClick = this.handleClick.bind(this)
+    this.fetchUserImage()
   }
 
   handleClick() {
@@ -17,11 +22,23 @@ class Header extends React.Component {
     })
   }
 
+  fetchUserImage() {
+    fetch(url)
+      .then((response) => {
+        response.json().then((res) => {
+          this.props.dispatch({
+            type: 'GET_USER_IMAGE',
+            value: res.profile_image
+          })
+        })
+      })
+  }
+
   render() {
     return(
       <div className='header'>
         <img
-          src='https://rr.img.naver.jp/mig?src=http%3A%2F%2Fcdn.mkimg.carview.co.jp%2Fminkara%2Fuserstorage%2F000%2F009%2F818%2F181%2F73ea5d0eff.jpg&twidth=1200&theight=1200&qlt=80&res_format=jpg&op=r'
+          src={this.props.state.profileImage}
           className='header__img'
           onClick={this.handleClick}
         />
--- front/src/reducers/layout/header.js
+++ front/src/reducers/layout/header.js
@@ -1,5 +1,6 @@
 const initialState = {
-  sidebarFlg: false
+  sidebarFlg: false,
+  profileImage: 'https://rr.img.naver.jp/mig?src=http%3A%2F%2Fcdn.mkimg.carview.co.jp%2Fminkara%2Fuserstorage%2F000%2F009%2F818%2F181%2F73ea5d0eff.jpg&twidth=1200&theight=1200&qlt=80&res_format=jpg&op=r'
 }
 
 export default (state = initialState, action) => {
@@ -7,6 +8,10 @@ export default (state = initialState, action) => {
     case 'CLICK_SIDEBAR':
       return Object.assign({}, state, {
         sidebarFlg: !state.sidebarFlg
+      })
+    case 'GET_USER_IMAGE':
+      return Object.assign({}, state, {
+        profileImage: action.value
       })
     default:
       return state
