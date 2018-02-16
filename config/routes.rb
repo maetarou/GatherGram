971150290e34e1741d00dcd423761013cdd858fb
t-kusakabe <ttmac0704@gmail.com>
2018-02-17 03:38:16 +0900

    Change routing path

--- config/routes.rb
+++ config/routes.rb
@@ -2,14 +2,14 @@ Rails.application.routes.draw do
   devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
   root 'tops#index'
 
-  get 'index', to: 'gathergram#index'
-  get 'ranking', to: 'rankings#index'
+  get '/gathergram', to: 'gathergram#index'
+  get '/ranking', to: 'rankings#index'
 
-  get 'users/auth/instagram/callback', to: 'users/omniauth_callbacks#instagram'
+  get '/users/auth/instagram/callback', to: 'users/omniauth_callbacks#instagram'
   get '/user', to: 'users#show'
-  get 'user/get_gather', to: 'users#get_gather'
-  get 'user/gather_history', to: 'users#gather_history'
-  get 'sessions/destroy'
+  get '/user/get_gather', to: 'users#get_gather'
+  get '/user/gather_history', to: 'users#gather_history'
+  get '/sessions/destroy'
   
   resources :tops, only: :index
   resources :rankings, only: :index
